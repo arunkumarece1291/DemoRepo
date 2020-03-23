2f8660b090e36c4a3852f688174d236299afd448
Arunkumar <arunkumarece1291@gmail.com>
2020-03-23 13:13:50 +0530

    my experiment in repo

---
+++ src/exp1/Prg1.java
@@ -0,0 +1,11 @@
+package exp1;
+
+public class Prg1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Exper for Prg 1");
+
+	}
+
+}
---
+++ src/exp1/Prg2.java
@@ -0,0 +1,11 @@
+package exp1;
+
+public class Prg2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Exp for Prg 2");
+
+	}
+
+}
